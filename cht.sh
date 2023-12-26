017d55755b5f7065c42b27ee57189207193207f8
JP <dragonginger10@gmail.com>
2023-12-26 13:27:42 -0600

    reorder the read statement

--- cht.sh
+++ cht.sh
@@ -3,7 +3,7 @@ languages=$(echo "golang lua cpp c typescript nodejs python" | tr ' ' '\n')
 core_utils=$(echo "awk xargs find sed " | tr ' ' '\n')
 
 selected=$(printf "%s" "$languages\n$core_utils" | fzf)
-read -pr "query: " query
+read -p "query: " -r query
 
 if printf "%s" "$languages"| grep -qs "$selected"; then
 	tmux neww bash -c "curl cht.sh/$selected/$(echo "$query" | tr ' ' '+') & while [ : ]; do sleep 1; done"
