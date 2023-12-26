0d33e368dff5a2cd7b1602b26961ebf148230557
JP <dragonginger10@gmail.com>
2023-12-26 13:17:38 -0600

    refactor using shell check

--- cht.sh
+++ cht.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-languages=`echo "golang lua cpp c typescript nodejs python" | tr ' ' '\n'`
-core_utils=`echo "awk xargs find sed " | tr ' ' '\n'`
+languages=$(echo "golang lua cpp c typescript nodejs python" | tr ' ' '\n')
+core_utils=$(echo "awk xargs find sed " | tr ' ' '\n')
 
-selected=`printf "$languages\n$core_utils" | fzf`
-read -p "query: " query
+selected=$(printf "%s" "$languages\n$core_utils" | fzf)
+read -pr "query: " query
 
-if printf $languages | grep -qs $selected; then
-	tmux neww bash -c "curl cht.sh/$selected/`echo $query | tr ' ' '+'` & while [ : ]; do sleep 1; done"
+if printf "%s" "$languages"| grep -qs "$selected"; then
+	tmux neww bash -c "curl cht.sh/$selected/$(echo "$query" | tr ' ' '+') & while [ : ]; do sleep 1; done"
 else
-	tmux neww bash -c "curl cht.sh/$selected~`echo $query | tr ' ' '+'` & while [ : ]; do sleep 1; done"
+	tmux neww bash -c "curl cht.sh/$selected~$(echo "$query" | tr ' ' '+') & while [ : ]; do sleep 1; done"
 fi
